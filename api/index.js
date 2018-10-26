820f2d5d66040f6468a5de414c6b48e4b3f11a1f
Jonathan Eustace <jeustace@tangledpixel.ca>
2018-10-26 08:38:23 -0700

    debugging

--- api/index.js
+++ api/index.js
@@ -1,35 +1,45 @@
 const fastify = require('fastify')({logger: true});
 const { Pool } = require('pg');
 const keys = require('./keys');
+const fastifyCors = require('fastify-cors');
+fastify.register(fastifyCors);
 
+// const pool = new Pool({
+//     user: keys.PG_USER,
+//     host: keys.PG_HOST,
+//     port: keys.PG_PORT,
+//     database: keys.PG_DATABASE,
+//     password: keys.PG_PASSWORD,
+// });
 
-const pool = new Pool({
-    user: keys.PG_USER,
-    host: keys.PG_HOST,
-    port: keys.PG_PORT,
-    database: keys.PG_DATABASE,
-    password: keys.PG_PASSWORD,
-});
-
-fastify.get('/value/:index', async (req, res) => {
+fastify.get('/value', async (req, res) => {
     try {
-        const response = await pool.query('SELECT name FROM nametable where $1', [req.params.index]);
-        console.log()
-        return response;
+        // const response = await pool.query('SELECT name FROM nametable where $1', [req.params.index]);
+
+      res.send({hi: 'hello'})
+        return {hi: 'hello'};
     } catch (err){
         console.log(err)
         // return fastify.log.error(err);
     }
 });
 
-fastify.post('/value/:name', async (req, res) => {
-   return await pool.query('INSERT INTO nametable (name) VALUES($1) RETURNING *', req.params.name);
-});
+// fastify.post('/value/:name', async (req, res) => {
+//    return await pool.query('INSERT INTO nametable (name) VALUES($1) RETURNING *', req.params.name);
+// });
 
-fastify.listen(5000, (err, address) => {
+fastify.get('*', () => {hi: 'hello'})
+
+fastify.listen(8081, async (err, address) => {
+  try {
+    // await pool.query('CREATE TABLE IF NOT EXISTS nametable (id serial, name varchar(255))');
     if (err) {
-        return fastify.log.error('Error starting: ', err);
+      return fastify.log.error('Error starting: ', err);
     }
 
     return fastify.log.info('Listening on: ', address);
-})
+  } catch (error){
+    console.log(error)
+  }
+
+});
