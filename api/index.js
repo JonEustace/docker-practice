71b379d40e24d4af390bfb8b973b67eb45b869d6
Jonathan Eustace <jeustace@tangledpixel.ca>
2018-10-26 11:27:07 -0700

    tiny clean up

--- api/index.js
+++ api/index.js
@@ -4,42 +4,53 @@ const keys = require('./keys');
 const fastifyCors = require('fastify-cors');
 fastify.register(fastifyCors);
 
-// const pool = new Pool({
-//     user: keys.PG_USER,
-//     host: keys.PG_HOST,
-//     port: keys.PG_PORT,
-//     database: keys.PG_DATABASE,
-//     password: keys.PG_PASSWORD,
-// });
+const pool = new Pool({
+    user: keys.PG_USER,
+    host: keys.PG_HOST,
+    port: keys.PG_PORT,
+    database: keys.PG_DATABASE,
+    password: keys.PG_PASSWORD,
+});
 
-fastify.get('/value', async (req, res) => {
+fastify.get('/values', async (req, res) => {
+  try {
+    const response = await pool.query('SELECT id, name FROM nametable');
+    res.type('application/json').code(200);
+    return { data: response.rows };
+  } catch (err){
+    return fastify.log.error(err);
+  }
+});
+
+fastify.get('/value/:index', async (req, res) => {
     try {
-        // const response = await pool.query('SELECT name FROM nametable where $1', [req.params.index]);
-
-      res.send({hi: 'hello'})
-        return {hi: 'hello'};
+      const response = await pool.query('SELECT name FROM nametable where id = $1', [req.params.index]);
+      res.type('application/json').code(200);
+      return { data: response.rows };
     } catch (err){
-        console.log(err)
-        // return fastify.log.error(err);
+      return fastify.log.error(err);
     }
 });
 
-// fastify.post('/value/:name', async (req, res) => {
-//    return await pool.query('INSERT INTO nametable (name) VALUES($1) RETURNING *', req.params.name);
-// });
-
-fastify.get('*', () => {hi: 'hello'})
-
-fastify.listen(8081, async (err, address) => {
+fastify.post('/value/:name', async (req, res) => {
   try {
-    // await pool.query('CREATE TABLE IF NOT EXISTS nametable (id serial, name varchar(255))');
+    const result = await pool.query('INSERT INTO nametable (name) VALUES($1) RETURNING *', [req.params.name]);
+    res.type('application/json').code(200);
+    return { data: result.rows }
+  } catch (err){
+    return fastify.log.error(err);
+  }
+});
+
+fastify.listen(5000, '0.0.0.0', async (err, address) => {
+  try {
+    await pool.query('CREATE TABLE IF NOT EXISTS nametable (id serial, name varchar(255))');
     if (err) {
       return fastify.log.error('Error starting: ', err);
     }
-
     return fastify.log.info('Listening on: ', address);
-  } catch (error){
-    console.log(error)
+  } catch (err){
+    return fastify.log.error(err);
   }
 
 });
